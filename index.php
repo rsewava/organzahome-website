89e83836f2674d0fd885e7c8715168e9d96fa52f
Rodney Sewava Isaac Tajir Mattar <r.sewava@gmail.com>
2026-01-08 13:53:58 +0300

    Added header main and footer

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html	lang	="en-GB">
+	<head>
+		<meta	charset	="utf-8"/>
+	</head>
+	<body>
+		<header>
+		</header>
+		<main>
+		</main>
+		<footer>
+			
+		</footer>
+	</body>
 </html>
