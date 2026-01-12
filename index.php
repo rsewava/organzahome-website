75b97f771a31fcff366c938445dab030b9d8dc0a
Rodney Sewava Isaac Tajir Mattar <r.sewava@gmail.com>
2026-01-12 07:54:17 +0300

    Added header html.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html	lang		="en-GB">
 	<head>
-		<link	href		="css/fonts.css"
+		<link	href		="css/colors.css"
 				rel			="stylesheet">
 		<link	href		="css/fonts.css"
 				rel			="stylesheet">
@@ -24,8 +24,156 @@
 			Skip to Main Content
 		</a>
 		<header>
-			<nav>
-			</nav>	
+			<nav data-name="Navbar" aria-label="Main" class="navbar navbar-expand-lg navbar-light o_colored_level o_cc d-none d-lg-block shadow-sm ">
+                <div id="o_main_nav" class="o_main_nav container">
+                    <a data-name="Navbar Logo" href="/" class="navbar-brand logo me-4">
+                        <span role="img" aria-label="Logo of Pretty Perfect" title="Pretty Perfect">
+                            <img src="/web/image/website/1/logo/Pretty%20Perfect?unique=766e51f" class="img img-fluid" width="95" height="40" alt="Pretty Perfect" loading="lazy"/>
+                        </span>
+                    </a>
+                    <ul role="menu" id="top_menu" class="nav navbar-nav top_menu o_menu_loading mx-auto">
+                        <li role="presentation" class="nav-item">
+                            <a role="menuitem" href="/shop/category/home-23" class="nav-link ">
+                                <span>Home Goods</span>
+                            </a>
+                        </li>
+                        <li role="presentation" class="nav-item">
+                            <a role="menuitem" href="/shop/category/jewelry-21" class="nav-link ">
+                                <span>Jewelry</span>
+                            </a>
+                        </li>
+                        <li role="presentation" class="nav-item">
+                            <a role="menuitem" href="/shop/category/accessories-22" class="nav-link ">
+                                <span>Accessories</span>
+                            </a>
+                        </li>
+                        <li role="presentation" class="nav-item">
+                            <a role="menuitem" href="/shop/category/sale-26" class="nav-link ">
+                                <span>SALE</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav align-items-center gap-2 flex-shrink-0 justify-content-end ps-3">
+                        <li class=" divider d-none"></li>
+                        <li class="o_wsale_my_cart  ">
+                            <a href="/shop/cart" aria-label="eCommerce cart" class="o_navlink_background btn position-relative rounded-circle p-1 text-center text-reset">
+                                <div class="">
+                                    <i class="fa fa-shopping-cart fa-stack"></i>
+                                    <sup class="my_cart_quantity badge bg-primary position-absolute top-0 end-0 mt-n1 me-n1 rounded-pill d-none" data-order-id="">0</sup>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="o_wsale_my_wish d-none  o_wsale_my_wish_hide_empty">
+                            <a href="/shop/wishlist" class="o_navlink_background btn position-relative rounded-circle p-1 text-center text-reset">
+                                <div class="">
+                                    <i class="fa fa-1x fa-heart fa-stack"></i>
+                                    <sup class="my_wish_quantity o_animate_blink badge bg-primary position-absolute top-0 end-0 mt-n1 me-n1 rounded-pill">0</sup>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="">
+                            <div class="modal fade css_editable_mode_hidden" id="o_search_modal" aria-hidden="true" tabindex="-1">
+                                <div class="modal-dialog modal-lg pt-5">
+                                    <div class="modal-content mt-5">
+                                        <form method="get" class="o_searchbar_form s_searchbar_input " action="/website/search" data-snippet="s_searchbar_input">
+                                            <div role="search" class="input-group input-group-lg">
+                                                <input type="search" name="search" class="search-query form-control oe_search_box border border-end-0 p-3 border-0 bg-light" placeholder="Search..." data-search-type="all" data-limit="5" data-display-image="true" data-display-description="true" data-display-extra-link="true" data-display-detail="true" data-order-by="name asc"/>
+                                                <button type="submit" aria-label="Search" title="Search" class="btn oe_search_button border border-start-0 px-4 bg-o-color-4">
+                                                    <i class="oi oi-search"></i>
+                                                </button>
+                                            </div>
+                                            <input name="order" type="hidden" class="o_search_order_by" value="name asc"/>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <a data-bs-target="#o_search_modal" data-bs-toggle="modal" role="button" title="Search" href="#" class="btn rounded-circle p-1 lh-1 o_navlink_background text-reset o_not_editable">
+                                <i class="oi oi-search fa-stack lh-lg"></i>
+                            </a>
+                        </li>
+                        <li data-name="Language Selector" class="o_header_language_selector ">
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <nav data-name="Navbar" aria-label="Mobile" class="navbar  navbar-light o_colored_level o_cc o_header_mobile d-block d-lg-none shadow-sm ">
+                <div class="o_main_nav container flex-wrap justify-content-between">
+                    <a data-name="Navbar Logo" href="/" class="navbar-brand logo ">
+                        <span role="img" aria-label="Logo of Pretty Perfect" title="Pretty Perfect">
+                            <img src="/web/image/website/1/logo/Pretty%20Perfect?unique=766e51f" class="img img-fluid" width="95" height="40" alt="Pretty Perfect" loading="lazy"/>
+                        </span>
+                    </a>
+                    <ul class="o_header_mobile_buttons_wrap navbar-nav flex-row align-items-center gap-2 mb-0">
+                        <li class=" divider d-none"></li>
+                        <li class="o_wsale_my_cart  ">
+                            <a href="/shop/cart" aria-label="eCommerce cart" class="o_navlink_background_hover btn position-relative rounded-circle border-0 p-1 text-reset">
+                                <div class="">
+                                    <i class="fa fa-shopping-cart fa-stack"></i>
+                                    <sup class="my_cart_quantity badge bg-primary position-absolute top-0 end-0 mt-n1 me-n1 rounded-pill d-none" data-order-id="">0</sup>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="o_wsale_my_wish d-none  o_wsale_my_wish_hide_empty">
+                            <a href="/shop/wishlist" class="o_navlink_background_hover btn position-relative rounded-circle border-0 p-1 text-reset">
+                                <div class="">
+                                    <i class="fa fa-1x fa-heart fa-stack"></i>
+                                    <sup class="my_wish_quantity o_animate_blink badge bg-primary position-absolute top-0 end-0 mt-n1 me-n1">0</sup>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="o_not_editable">
+                            <button class="nav-link btn me-auto p-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#top_menu_collapse_mobile" aria-controls="top_menu_collapse_mobile" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                        </li>
+                    </ul>
+                    <div id="top_menu_collapse_mobile" class="offcanvas offcanvas-end o_navbar_mobile">
+                        <div class="offcanvas-header justify-content-end o_not_editable">
+                            <button type="button" class="nav-link btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body d-flex flex-column justify-content-between h-100 w-100">
+                            <ul class="navbar-nav">
+                                <li class="">
+                                    <form method="get" class="o_searchbar_form s_searchbar_input " action="/website/search" data-snippet="s_searchbar_input">
+                                        <div role="search" class="input-group mb-3">
+                                            <input type="search" name="search" class="search-query form-control oe_search_box rounded-start-pill text-bg-light ps-3 border-0 bg-light" placeholder="Search..." data-search-type="all" data-limit="0" data-display-image="true"data-display-description="true" data-display-extra-link="true" data-display-detail="true" data-order-by="name asc"/>
+                                            <button type="submit" aria-label="Search" title="Search" class="btn oe_search_button rounded-end-pill bg-o-color-3 pe-3">
+                                                <i class="oi oi-search"></i>
+                                            </button>
+                                        </div>
+                                        <input name="order" type="hidden" class="o_search_order_by" value="name asc"/>
+                                    </form>
+                                </li>
+                                <ul role="menu" class="nav navbar-nav top_menu  text-center">
+                                    <li role="presentation" class="nav-item">
+                                        <a role="menuitem" href="/shop/category/home-23" class="nav-link ">
+                                            <span>Home Goods</span>
+                                        </a>
+                                    </li>
+                                    <li role="presentation" class="nav-item">
+                                        <a role="menuitem" href="/shop/category/jewelry-21" class="nav-link ">
+                                            <span>Jewelry</span>
+                                        </a>
+                                    </li>
+                                    <li role="presentation" class="nav-item">
+                                        <a role="menuitem" href="/shop/category/accessories-22" class="nav-link ">
+                                            <span>Accessories</span>
+                                        </a>
+                                    </li>
+                                    <li role="presentation" class="nav-item">
+                                        <a role="menuitem" href="/shop/category/sale-26" class="nav-link ">
+                                            <span>SALE</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </ul>
+                            <ul class="navbar-nav gap-2 mt-3 w-100">
+                                <li data-name="Language Selector" class="o_header_language_selector "></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
 		</header>
 		<main	id			="maincontent">
 		</main>
